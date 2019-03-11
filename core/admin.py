761d03153aa70862a99867c42b105468095ccd7d
AlanaKaimi <ajbryce@live.com>
2019-03-11 14:43:51 -0400

    Admin site up and running, created superuser, registered, Advanced Configuration

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from core.models import Book, Author, Category
 
 # Register your models here.
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Category)
+
+
+class BooksInline(admin.TabularInline):
+    model = Book
+    extra = 0
+
+# Define the admin class
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name')
+    fields = ['first_name', 'last_name']
+    inlines = [BooksInline]
+
+# Register the Admin classes for Book using the decorator
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    # inlines = [BooksInstanceInline] <-- Not sure if I need this here
