8cf679e0ae0f45684a1a65c58a613c74dbc702e0
AlanaKaimi <ajbryce@live.com>
2019-03-14 12:12:37 -0400

    trying to deploy yo heroku

--- freeshelf/settings.py
+++ freeshelf/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
@@ -93,6 +95,17 @@ DATABASES = {
     }
 }
 
+## For Postgresql:
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'freeshelf',
+#         'USER': 'freeshelf',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
@@ -143,3 +156,5 @@ LOGIN_REDIRECT_URL = '/'
 
 # Email
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+django_heroku.settings(locals())
